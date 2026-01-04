fa9653c7ae914f2479d3b254c85c72c9dc816745
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-04 13:32:19 +0100

    feat: mostrar título y limpiar resumen antes de actualizar

--- js/app.js
+++ js/app.js
@@ -139,6 +139,9 @@ function agregarPlatillo(producto) {
         cliente.pedido = [...resultado]
     }
 
+    // Limpiar el código HTML previo
+    limpiarHTML();
+
     // Mostrar el Resumen
     actualizarResumen();
 }
@@ -147,7 +150,7 @@ function actualizarResumen() {
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('div');
-    resumen.classList.add('col-md-6');
+    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
     
     // Información de la mesa 
     const mesa = document.createElement('p');
@@ -171,6 +174,24 @@ function actualizarResumen() {
     mesa.appendChild(mesaSpan);
     hora.appendChild(horaSpan);
 
-    contenido.appendChild(mesa);
-    contenido.appendChild(hora);
+    // Titulo de la sección
+    const heading = document.createElement('h3');
+    heading.textContent = 'Platillos Consumidos';
+    heading.classList.add('my-4', 'text-center');
+
+    // Iterar sobre el array de pedidos
+
+    // Agergar el contenido
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora);
+    resumen.appendChild(heading);
+
+    contenido.appendChild(resumen);
+}
+
+function limpiarHTML() {
+    const contenido = document.querySelector('#resumen .contenido');
+    while(contenido.firstChild) {
+        contenido.removeChild(contenido.firstChild);
+    }
 }

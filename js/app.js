e6a417fb6a763307d8db3305db19c361398f95fc
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-05 16:55:34 +0100

    feat: agregar formulario de propinas al resumen

--- js/app.js
+++ js/app.js
@@ -154,7 +154,7 @@ function actualizarResumen() {
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('div');
-    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
+    resumen.classList.add('col-md-6', 'card', 'py-2', 'px-3', 'shadow');
 
     // Información de la mesa 
     const mesa = document.createElement('p');
@@ -252,12 +252,15 @@ function actualizarResumen() {
     })
 
     // Agergar el contenido
+    resumen.appendChild(heading);
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
-    resumen.appendChild(heading);
     resumen.appendChild(grupo);
 
     contenido.appendChild(resumen);
+
+    // Mostar Formulario de Propinas
+    formularioPropinas();
 }
 
 function limpiarHTML() {
@@ -300,4 +303,24 @@ function mensajePedidoVacio() {
     texto.textContent = 'Añade los elementos del pedido';
 
     contenido.appendChild(texto);
+}
+
+function formularioPropinas() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const formulario = document.createElement('div');
+    formulario.classList.add('col-md-6', 'formulario');
+
+    const divFormulario = document.createElement('div');
+    divFormulario.classList.add('card', 'py-2', 'px-3', 'shadow');
+
+    const heading = document.createElement('h3');
+    heading.classList.add('my-4', 'text-center');
+    heading.textContent = 'Propina';
+
+    formulario.appendChild(divFormulario);
+
+    divFormulario.appendChild(heading);
+
+    contenido.appendChild(formulario);
 }
